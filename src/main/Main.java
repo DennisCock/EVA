df8d4411d7b413c7603257963d562ede666eef79
Sana De Clippel <Sana De Clippel@172.18.191.224>
2016-02-25 08:39:36 +0100

    Aanmaken project

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sana De Clippel
+ */
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
