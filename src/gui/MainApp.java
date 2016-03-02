727cdcb29367b8e3ec8a1c8c37047973b7478db5
Sana De Clippel <Sana De Clippel@172.18.191.224>
2016-03-02 08:28:29 +0100

    GUI LoginScherm

--- src/gui/MainApp.java
+++ src/gui/MainApp.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
@@ -21,47 +22,14 @@ import javafx.stage.Stage;
  * @author Sana De Clippel
  */
 public class MainApp extends Application {
-    
-    private Stage primaryStage;
-    private AnchorPane LoginScherm;
 
     @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("EVA");
-
-        initLoginScherm();
-    }
-
-    /**
-     * Initializes the root layout.
-     */
-    public void initLoginScherm() {
-        try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("LoginScherm.fxml"));
-            LoginScherm = (AnchorPane) loader.load();
-
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(LoginScherm);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Returns the main stage.
-     * @return
-     */
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
-    public static void main(String[] args) {
-        launch(args);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/LoginScherm.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Login EVA");
+        stage.show();
     }
     
 }
